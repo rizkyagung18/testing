5f6909959e83e193ef2cc377b2dbeb8ee12d816f
rizkyagung18 <agungrizky1805@gmail.com>
2020-09-15 21:25:01 +0700

    update 3

--- 3a.js
+++ 3a.js
@@ -1,4 +1,5 @@
 const jackpot = new Promise((resolve, reject) => {
+   setTimeout(() => {
     const random = Math.random() * 10
     const hadiah = ['sepatu', 'hoodie', 'macbook']
     if(random > 7) {
@@ -6,6 +7,7 @@ const jackpot = new Promise((resolve, reject) => {
     } else {
         reject(new Error("Unfortunately, no gift"))
     }
+   }, 3000)
 })
 
 jackpot.then((hadiah) => {
@@ -15,3 +17,89 @@ jackpot.then((hadiah) => {
     console.log(err.message)
 })
 
+const faker = (url, opt) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const req = {
+                '/users': [
+                    {
+                        id: 1,
+                        name: 'Pak Soleh',
+                        age: 35,
+                        city: 'Jakarta',
+                        covid19: true
+                    },
+                    {
+                        id: 2,
+                        name: 'Arung',
+                        age: 30,
+                        city: 'Bandung',
+                        covid19: false
+                    },
+                    {
+                        id: 3,
+                        name: 'Hamzah',
+                        age: 24,
+                        city: 'Tangerang',
+                        covid19: false
+                    },
+                    {
+                        id: 4,
+                        name: 'Rikara',
+                        age: 20,
+                        city: 'Jakarta',
+                        covid19: true
+                    },
+                    {
+                        id: 5,
+                        name: 'Ucup',
+                        age: 20,
+                        city: 'Bay Area',
+                        covid19: false
+                    }
+                ],
+                '/movies': [
+                    {
+                        id: 101,
+                        title: 'Ikan Hiu Makan Tomat',
+                        rating: 7.95
+                    },
+                    {
+                        id: 102,
+                        title: 'Cinta Tyranosaurus',
+                        rating: 8.5
+                    },
+                    {
+                        id: 103,
+                        title: 'Ada Apa Dengan Coding',
+                        rating: 9
+                    }
+                ]
+            }
+            const datas = req[url]
+            if(opt) {
+                const data = req[url].map(item => item[opt]) 
+                if(data.some(item => item == undefined)) {
+                    reject({ status: 404, message: 'DATA NOT FOUND!!'})
+                } else {
+                    resolve(data)
+                }
+            }
+            if(datas) {
+                resolve(datas)
+            } else {
+                reject({ status: 404, message: 'DATA NOT FOUND!!'})
+            }
+        }, 3000);
+    })
+}
+
+faker('/movies')
+ .then(data => {
+     console.log('Congrats you got a data')
+     console.log(data)
+ })
+ .catch(err => {
+     console.log(`Error Status: ${err.status}`)
+     console.log(err.message)
+ })
